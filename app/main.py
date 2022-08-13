5e542d35ec8abc0325ce8240fa8c66907d994c8a
nickL <nick@nickLs-MacBook-Air.local>
2022-08-12 21:17:39 -0500

    added cors, front end is next

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from .routers import workout, user, auth, nutrition
@@ -6,7 +7,12 @@ from .routers import workout, user, auth, nutrition
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 app.include_router(workout.router)
 app.include_router(user.router)
